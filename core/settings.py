4bb15acf362e331f51d66f90ed853422979994a3
svyatkozych <sviatoslav.kozych.24@pnu.edu.ua>
2026-04-29 10:15:07 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dymok-gastropub.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['dym0k-gastro-pub.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
